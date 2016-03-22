df0a6b3e836c63fc61ef06a139b29ec09abf6eec
Mijlandst <s.t.mijland@student.rug.nl>
2016-03-22 17:27:01 +0100

    Updated the RMI call to send its message to the DBQueue

--- Translit/src/main/java/rmi/client/LaunchCalcTranslationtask.java
+++ Translit/src/main/java/rmi/client/LaunchCalcTranslationtask.java
@@ -1,11 +1,22 @@
 package rmi.client;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.netcomputing.webservices.datamodel.Text;
 import org.netcomputing.webservices.queues.q4user.DBQueue;
+import org.netcomputing.webservices.queues.q4user.PacketFormatException;
+import org.netcomputing.webservices.queues.q4user.TranslatedPacket;
+import org.netcomputing.webservices.queues.q4user.TranslationPacket;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import rmi.base.Compute;
 import rmi.base.RmiStarter;
@@ -41,8 +52,27 @@ public class LaunchCalcTranslationtask extends RmiStarter {
 			/**
 			 * Send solution to DBQueue
 			 */
-			DBQueue us = new DBQueue();
-			us.updateDatabase();
+		
+			ConnectionFactory factory = new ConnectionFactory();
+		    factory.setHost("localhost"); //TODO: configure?
+		    try {
+				Connection connection = factory.newConnection();
+				Channel channel = connection.createChannel();
+				
+				TranslatedPacket trans = new TranslatedPacket(langFrom, langTo,
+						translation.getMessage(), msg.getMessage());
+				
+				channel.queueDeclare(DBQueue.DB, true, false, false, null);
+				channel.basicPublish("", DBQueue.DB, null, trans.getPacket().getBytes("UTF-8"));
+				
+				channel.close();
+				connection.close();
+		    } catch (IOException e) {
+		    	logger.log(Level.SEVERE, "Reception failed", e);
+			} catch (TimeoutException e) {
+				logger.log(Level.SEVERE, "Reception timed out", e);
+			}
+			
 			logger.log(Level.INFO, "DBQueue update() called by the AI...");
 		} catch (Exception e) {
 			e.printStackTrace();
